21ba028935c99a13c91706349a06ec4c4c9b01c3
Shimon Berger <jadeboy7@gmail.com>
2018-10-07 17:02:52 -0400

    ensure inputs are error-free and in the proper range

--- src/Babysitter.java
+++ src/Babysitter.java
@@ -6,16 +6,37 @@ public class Babysitter {
         Scanner scanner = new Scanner(System.in);
 
         // start time
-        System.out.print("Enter start time: ");
-        int start = scanner.nextInt();
+        int start;
+        do {
+            System.out.print("Enter start time (between 5 P.M. and 4 A.M.): ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Sorry, please enter a number: ");
+                scanner.next();
+            }
+            start = scanner.nextInt();
+        } while (start < 4 || start > 12);
 
         // end time
-        System.out.print("Enter end time: ");
-        int end = scanner.nextInt();
+        int end;
+        do {
+            System.out.print("Enter end time (between 5 P.M. and 4 A.M.): ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Sorry, please enter a number: ");
+                scanner.next();
+            }
+            end = scanner.nextInt();
+        } while (end < 4 || end > 12);
 
         // bedtime
-        System.out.print("Enter bedtime: ");
-        int bed = scanner.nextInt();
+        int bed;
+        do {
+            System.out.print("Enter bedtime (between 5 P.M. and 4 A.M.): ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Sorry, please enter a number: ");
+                scanner.next();
+            }
+            bed = scanner.nextInt();
+        } while (bed < 4 || bed > 12);
 
         calculateCharge(start, end, bed);
     }
