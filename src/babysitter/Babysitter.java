f8151cb6220aca247505712953787793a6f4d4b7
Shimon Berger <jadeboy7@gmail.com>
2018-10-08 02:25:06 -0400

    implement initial calculation logic fix for input range bug

--- src/babysitter/Babysitter.java
+++ src/babysitter/Babysitter.java
@@ -39,25 +39,64 @@ public class Babysitter {
                 scanner.next();
             }
             value = scanner.nextInt();
-        } while (value < 4 || value > 12);
+        } while (value < 1 || value > 12);
 
         return value;
     }
 
     private static void calculateCharge(int start, int end, int bed) throws InvalidInputException {
-        List<Integer> timeArray = Arrays.asList(5,6,7,8,9,10,11,12,1,2,3,4);
-        int startIndex, endIndex, bedIndex;
+        final List<Integer> timeArray = Arrays.asList(5,6,7,8,9,10,11,12,1,2,3,4);
+        final int START_TO_BED = 12, BED_TO_MIDNIGHT = 8, MIDNIGHT_TO_END = 16;
+
+        int startIndex, endIndex, bedIndex, midnightIndex;
         startIndex = timeArray.indexOf(start);
         endIndex = timeArray.indexOf(end);
         bedIndex = timeArray.indexOf(bed);
+        midnightIndex = timeArray.indexOf(12);
 
         // check for invalid start/end time
         if (endIndex <= startIndex) {
             throw new InvalidInputException("Sorry, you entered invalid start/end times.");
         }
 
-        System.out.println("Start Index: " + startIndex +
-                            ", End Index: " + endIndex +
-                            ", Bed Index: " + bedIndex);
+        /*
+        Hourly rates:
+            - $12/hour from start-time to bedtime
+            - $8/hour from bedtime to midnight
+            - $16/hour from midnight to end of job
+
+         Priority:
+            1. Midnight to end: always $16/hour (regardless of bedtime)
+            2a. Start time to bedtime, if start time is before midnight and bedtime is before/at midnight: $12/hour
+            2b. Start time to midnight, if start time is before midnight and bedtime is after midnight: $12/hour
+            3a. Bedtime to end time, if bedtime is before midnight and end time is before/at midnight: $8/hour
+            3b. Bedtime to midnight, if bedtime is before midnight and end time is after midnight: $8/hour
+         */
+        int totalCharge = 0;
+
+        // 1.
+        totalCharge += Math.max(0, endIndex - midnightIndex) * MIDNIGHT_TO_END;
+
+        // 2a.
+        if (startIndex < midnightIndex && bedIndex <= midnightIndex) {
+            totalCharge += Math.max(0, bedIndex - startIndex) * START_TO_BED;
+        }
+
+        // 2b.
+        if (startIndex < midnightIndex && bedIndex > midnightIndex) {
+            totalCharge += Math.max(0, midnightIndex - startIndex) * START_TO_BED;
+        }
+
+        // 3a.
+        if (bedIndex < midnightIndex && endIndex <= midnightIndex) {
+            totalCharge += Math.max(0, endIndex - bedIndex) * BED_TO_MIDNIGHT;
+        }
+
+        // 3b.
+        if (bedIndex < midnightIndex && endIndex > midnightIndex) {
+            totalCharge += Math.max(0, midnightIndex - bedIndex) * BED_TO_MIDNIGHT;
+        }
+
+        System.out.println("Total Charge: " + totalCharge);
     }
 }
